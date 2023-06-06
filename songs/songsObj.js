7ebf1e1cf33f370d8bbfc724a00af31941eda215
UncompiledCollective <krzeslermax@gmail.com>
2023-06-06 23:48:22 +0200

    Fixing memory heap issues

--- songs/songsObj.js
+++ songs/songsObj.js
@@ -31,7 +31,6 @@ const songsObj = {
         
     }
 }
-songsObj.add("Anri - DRIVING MY LOVE",".mp3")
 songsObj.add("Mariya Takeuchi - 中森明菜 OH NO, OH YES! (cover)",".mp3")
 songsObj.add("Anri - Shyness boy",".mp3")
 module.exports = {
